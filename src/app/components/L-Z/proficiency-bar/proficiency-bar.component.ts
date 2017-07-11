263f65dde60bcf72f0747cb922f46ea99497d526
Doug Cooper <doug.cpr@gmail.com>
2017-07-11 11:51:41 -0400

    updated portoflio icons and added in data to mongodb

--- src/app/components/L-Z/proficiency-bar/proficiency-bar.component.ts
+++ src/app/components/L-Z/proficiency-bar/proficiency-bar.component.ts
@@ -9,7 +9,7 @@ export class ProficiencyBarComponent implements OnInit {
 
   @Input() title: string;
   @Input() proficiency: string;
-  @Input() icon: string;
+  @Input() icon: [string];
 
   constructor(
   ) { }

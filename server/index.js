4dd377971925009bbfa5f3544279c98899d74a62
Doug Cooper <doug.cpr@gmail.com>
2017-08-03 10:03:34 -0500

    removed project model

--- server/index.js
+++ server/index.js
@@ -56,31 +56,6 @@ app.get("/getProjectData/about-me-data", function(req, res) {
   });
 });
 
-let storage = multer.diskStorage({ //multers disk storage settings
-  destination: function (req, file, cb) {
-    console.log("set file destination");
-      cb(null, './uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-let upload = multer({ //multer settings
-  storage: storage
-}).single('file');
-
-/** API path that will upload the files */
-app.post('/uploads', function(req, res) {
-  upload(req,res,function(err){
-    if(err){
-      res.json({error_code:1,err_desc:err});
-      return;
-    }
-    res.json({error_code:0,err_desc:null});
-  });
-});
-
 app.listen('3000', function(){
   console.log('running on 3000...');
 });

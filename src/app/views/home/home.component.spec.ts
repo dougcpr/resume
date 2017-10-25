2870bcbd2ea887d30c523dde4d8fcc032b1b695f
Doug Cooper <doug.cpr@gmail.com>
2017-10-25 15:28:01 -0500

    updatd card styling to fit content

--- src/app/views/home/home.component.spec.ts
+++ src/app/views/home/home.component.spec.ts
@@ -7,15 +7,18 @@ describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
+    /* Configure the testing bed with the components that should be tested */
     TestBed.configureTestingModule({
       declarations: [ HomeComponent ]
     })
+    /* compile components asynchronously because this creation is slow */
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    /* This will run detect changes which is normally run in the background in dev */
     fixture.detectChanges();
   });
 
